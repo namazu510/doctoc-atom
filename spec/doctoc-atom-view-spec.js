4fec204af73610f8d73b21bc0fa57d6144a871d3
namaz <namazu510@hotmail.co.jp>
2017-04-11 22:06:24 +0900

    first-commit

---
+++ spec/doctoc-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import DoctocAtomView from '../lib/doctoc-atom-view';
+
+describe('DoctocAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
